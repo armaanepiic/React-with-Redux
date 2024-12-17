5c79ac6dc67eaa956853beb79ed0195de0bbca5a
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2024-12-17 11:59:50 +0600

    npm practice

---
+++ MyApp/index.js
@@ -0,0 +1,6 @@
+let format = require('date-format');
+// let time = format("hh:mm:ss", new Date());
+let time = format("dd/MM/yyyy", new Date());
+
+
+console.log(time);
---
+++ README.md
@@ -0,0 +1,2 @@
+# React-with-Redux
+Here I am practicing React with Redux
