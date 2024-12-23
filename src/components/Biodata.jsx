adb827104e88a3e9fffd0c4074cdf32065c0996a
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-23 13:48:24 +0600

    biodata updated

--- src/components/Biodata.jsx
+++ src/components/Biodata.jsx
@@ -1,18 +1,18 @@
 function Biodata(obj) {
-    console.log(obj);
-    
+  console.log(obj);
+    const { name, age, mobile, occupation, skills, interests } = obj;
   return (
     <div className="biodata">
-      <h1>Biodata of {obj.name}</h1>
+      <h1>Biodata of {name}</h1>
       <div className="personal-info">
-        <p>Age: {obj.age}</p>
-        {obj.mobile && <p>Mobile: {obj.mobile}</p>}
-        <p>Occupation: {obj.occupation}</p>
+        <p>Age: {age}</p>
+        {mobile && <p>Mobile: {mobile}</p>}
+        <p>Occupation: {occupation}</p>
       </div>
       <div className="skills">
         <h2>My Skills</h2>
         <ul>
-          {obj?.skills?.map((skill) => (
+          {skills?.map((skill) => (
             <li key={skill}>{skill}</li>
           ))}
         </ul>
@@ -20,7 +20,7 @@ function Biodata(obj) {
       <div className="interests">
         <h2>My Interests</h2>
         <ul>
-          {obj.interests.map((interest) => (
+          {interests.map((interest) => (
             <li key={interest}>{interest}</li>
           ))}
         </ul>
