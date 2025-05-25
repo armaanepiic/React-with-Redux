de13d48cde45950fc8154e2052257f9d42061ee6
Arman Hossain <armaanepiicfb17@gmail.com>
2025-05-25 15:35:53 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState, useCallback } from "react";
+import { useState, useCallback, usem, useMemo } from "react";
 import OurAppTitle from "./components/OurAppTitle";
 import Button from "./components/Button";
 import Title from "./components/Title";
@@ -15,7 +15,13 @@ export default function App() {
   
   const increaseHandler2 = useCallback(() => {
     setCounter2((prev) => prev + 5);
-  },[]);
+  }, []);
+  
+  const type = useMemo(() => {
+    let i = 0;
+    while (i < 1000000000) i++;
+    return counter1 % 2 == 0 ? `Counter is Even` : `Counter is Odd`;
+  },[counter1]);
 
   console.log("App is rendering");
   
@@ -25,6 +31,7 @@ export default function App() {
       <hr />
       <div className="counter-app-1">
         <Title value={counter1} />
+        <p>{type}</p>
         <Button clickHandler={increaseHandler1} />
       </div>
       <hr />
